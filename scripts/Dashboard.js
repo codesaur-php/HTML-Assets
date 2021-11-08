8cb9fdd9290c88997c8a2f1011f7a824ecc72dca
Наранхүү Н <codesaur@gmail.com>
2021-11-08 12:01:06 +0800

    Update Dashboard.js

--- scripts/Dashboard.js
+++ scripts/Dashboard.js
@@ -1,21 +1,7 @@
 /* global AjaxModal */
 var Dashboard = function() {
-    function include(file) {  
-      var script = document.createElement('script');
-      script.src = file;
-      script.defer = true;
-      document.getElementsByTagName('head').item(0).appendChild(script);
-    }
-    
     return {
         init: function(modalId, loadingText) {            
-            include('https://cdn.jsdelivr.net/gh/codesaur-php/HTML-Assets@2.0/scripts/spinner.js');
-            include('https://cdn.jsdelivr.net/gh/codesaur-php/HTML-Assets@2.0/scripts/ScrollUp.js');
-            include('https://cdn.jsdelivr.net/gh/codesaur-php/HTML-Assets@2.0/scripts/NotifyTop.js');
-            include('https://cdn.jsdelivr.net/gh/codesaur-php/HTML-Assets@2.0/scripts/AjaxModal.js');
-            include('https://cdn.jsdelivr.net/gh/codesaur-php/HTML-Assets@2.0.1/scripts/imageInput.js');
-            include('https://cdn.jsdelivr.net/gh/codesaur-php/HTML-Assets@2.0.1/scripts/SubmitForm.js');
-
             if (!String.prototype.format) {
               String.prototype.format = function() {
                 var args = arguments;
@@ -25,13 +11,11 @@ var Dashboard = function() {
               };
             }
             
-            document.addEventListener('DOMContentLoaded', function() {
-                const loader = '<span class="spinner-border spinner-border-sm text-secondary" role="status" aria-hidden="true" style="margin-right:5px;top:1px;position:relative"></span> ' + loadingText + ' ...';
+            const loader = '<span class="spinner-border spinner-border-sm text-secondary" role="status" aria-hidden="true" style="margin-right:5px;top:1px;position:relative"></span> ' + loadingText + ' ...';
             
-                AjaxModal.append(modalId, loader);
+            AjaxModal.append(modalId, loader);
 
-                ScrollUp();
-            });
+            ScrollUp();
         },
         
         activateLink: function(link) {
