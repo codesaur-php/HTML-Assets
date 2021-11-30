68369dd8e23cd421427dae88e675022698719f05
Наранхүү Н <codesaur@gmail.com>
2021-11-30 13:36:17 +0800

    Update SubmitForm.js

--- scripts/SubmitForm.js
+++ scripts/SubmitForm.js
@@ -37,77 +37,80 @@ var SubmitForm = function(submittersSelector, formSelector, callbackSuccess, cal
                 }
             }
             
-            form.addEventListener('submit', function (event) {
-                event.preventDefault();
+            if (!form.hasAttribute('hasSubmitHandler')) {
+                form.addEventListener('submit', function (event) {
+                    event.preventDefault();
 
-                const _valid = this.checkValidity();
-                this.classList.add('was-validated');
-                if (!_valid) {
-                    event.stopPropagation();
+                    const _valid = this.checkValidity();
+                    this.classList.add('was-validated');
+                    if (!_valid) {
+                        event.stopPropagation();
 
-                    stopSubmitters();
-
-                    let response = {
-                        type: 'validation-error',
-                        title: textError,
-                        error: {
-                            message: textFormValidationError
-                        }
-                    };
-                    if (callbackError) {
-                        return callbackError(response);
-                    }
-                    return NotifyTop('danger', response.title, response.error.message);
-                } else {
-                    const url = this.getAttribute('action');
-                    let method = this.getAttribute('method');
-                    if (!method) {
-                        method = 'POST';
-                    }
-                    const data = new FormData(this);
-                    fetch(url, {
-                        method: method,
-                        body: data
-                    }).then(res => {
                         stopSubmitters();
 
-                        if (!res.ok) {
-                            throw new Error(res.statusText);
-                        }
-
-                        return res.json();
-                    }).then(response => {
-                        if (response.status !== 'success') {
-                            throw new Error(response.message ? response.message : 'Invalid response!');
-                        }
-
-                        if (callbackSuccess) {
-                            return callbackSuccess(response);
-                        }
-
-                        let type = response.type ? response.type : 'success';
-                        let title = response.title ? response.title : textSuccess;
-                        NotifyTop(type, title, response.message ? response.message : '');
-
-                        if (response.href
-                                && response.href !== 'javascript:;'
-                        ) {
-                            window.location.href = response.href;
-                        }
-                    })
-                    .catch(error => {
                         let response = {
-                            type: 'error-error',
+                            type: 'validation-error',
                             title: textError,
-                            error: error
-                        };                            
+                            error: {
+                                message: textFormValidationError
+                            }
+                        };
                         if (callbackError) {
-                            return callbackError(error);
+                            return callbackError(response);
                         }
                         return NotifyTop('danger', response.title, response.error.message);
-                    });
-                }
-            }, false);
+                    } else {
+                        const url = this.getAttribute('action');
+                        let method = this.getAttribute('method');
+                        if (!method) {
+                            method = 'POST';
+                        }
+                        const data = new FormData(this);
+                        fetch(url, {
+                            method: method,
+                            body: data
+                        }).then(res => {
+                            stopSubmitters();
+
+                            if (!res.ok) {
+                                throw new Error(res.statusText);
+                            }
+
+                            return res.json();
+                        }).then(response => {
+                            if (response.status !== 'success') {
+                                throw new Error(response.message ? response.message : 'Invalid response!');
+                            }
+
+                            if (callbackSuccess) {
+                                return callbackSuccess(response);
+                            }
+
+                            let type = response.type ? response.type : 'success';
+                            let title = response.title ? response.title : textSuccess;
+                            NotifyTop(type, title, response.message ? response.message : '');
+
+                            if (response.href
+                                    && response.href !== 'javascript:;'
+                            ) {
+                                window.location.href = response.href;
+                            }
+                        })
+                        .catch(error => {
+                            let response = {
+                                type: 'error-error',
+                                title: textError,
+                                error: error
+                            };                            
+                            if (callbackError) {
+                                return callbackError(error);
+                            }
+                            return NotifyTop('danger', response.title, response.error.message);
+                        });
+                    }
+                }, false);
+                form.setAttribute('hasSubmitHandler', true);
+            }
             
             const submit = form.ownerDocument.createElement('input');
             submit.style.display = 'none';
